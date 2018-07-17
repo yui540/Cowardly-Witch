d219a31cbe270980f685bc13843b8d234fa932bd
yui540 <tabletennis540@gmail.com>
2018-07-17 15:34:58 +0900

    add: 初期環境を構築

---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const mode = process.env.MODE
+
+module.exports = {
+  mode,
+  entry: {
+    app: './src/scripts/app.js',
+  },
+  output: {
+    path: `${ __dirname }/public/scripts`,
+    filename: 'app.min.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: (mode === 'development')
+              }
+            },
+            'sass-loader',
+            'import-glob-loader'
+          ]
+        })
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader',
+          {
+            loader: 'eslint-loader',
+            options: {
+              fix: false,
+              failOnError: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 51200,
+              name: './images/[name].[ext]'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('../stylesheets/style.css')
+  ],
+  devServer: {
+    contentBase: `${ __dirname }/public`,
+    port: 3000,
+    open: true
+  }
+}
