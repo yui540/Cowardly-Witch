be7b5c427cb818d581e8945f0a7e306c2c21d842
yui540 <tabletennis540@gmail.com>
2018-07-27 22:59:18 +0900

    add: version_1のstage1を実装

--- src/scripts/components/Version1.js
+++ src/scripts/components/Version1.js
@@ -4,5 +4,22 @@ import VersionView from './VersionView'
 export default () => (
   <div className="version-1">
     <VersionView version="1" />
+    <div className="version-1__stage">
+      <div className="version-1__stage-1">
+        <div className="version-1__fantasy">
+          <div className="bg"></div>
+          <div className="bg-img"></div>
+          <div className="silhouette">
+            <div></div>
+          </div>
+          <div className="message">
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+      </div>
+      <div className="version-1__stage-2"></div>
+    </div>
   </div>
 )
