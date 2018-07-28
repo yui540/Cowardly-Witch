ccd39e29ff65da273bfcc3a17b0a03ee67b78b9b
yui540 <tabletennis540@gmail.com>
2018-07-29 08:46:01 +0900

    update: stage-2を実装

--- src/scripts/components/Version1.js
+++ src/scripts/components/Version1.js
@@ -19,7 +19,51 @@ export default () => (
           </div>
         </div>
       </div>
-      <div className="version-1__stage-2"></div>
+      <div className="version-1__stage-2">
+        <div className="bg"></div>
+        <div className="toiro"></div>
+        <div className="bullet">
+          <div className="line">
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+          <div className="label-1"></div>
+          <div className="label-2"></div>
+        </div>
+        <div className="wall">
+          <div className="left">
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+          <div className="text"></div>
+          <div className="right">
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+        <div className="message">
+          <div></div>
+          <div></div>
+        </div>
+        <div className="word">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="end">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
     </div>
   </div>
 )
