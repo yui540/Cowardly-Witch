98e6042826e6eb7e62d6988e67f658c9d68eff91
yui540 <tabletennis540@gmail.com>
2018-08-06 19:56:36 +0900

    update: stage-1をマークアップ

--- src/scripts/components/Version2.js
+++ src/scripts/components/Version2.js
@@ -4,5 +4,49 @@ import VersionView from './VersionView'
 export default () => (
   <div className="version-2">
     <VersionView version="2" />
+    <div className="version-2__bg">
+      <div></div><div></div><div></div><div></div>
+    </div>
+    <div className="version-2__stage-1">
+      <div className="bar">
+        <div></div><div></div><div></div><div></div><div></div>
+      </div>
+      <div className="text"></div>
+      <div className="circle">
+        <div></div><div></div><div></div><div></div>
+      </div>
+      <div className="shutter">
+        <section>
+          <div></div><div></div><div></div><div></div><div></div>
+          <div></div><div></div><div></div><div></div><div></div>
+        </section>
+        <section>
+          <div></div><div></div><div></div><div></div><div></div>
+          <div></div><div></div><div></div><div></div><div></div>
+        </section>
+      </div>
+      <div className="poster">
+        <div className="poster-1"></div>
+        <div className="poster-2">
+          <div className="line-1"></div>
+          <div className="line-2"></div>
+        </div>
+        <div className="poster-3"></div>
+      </div>
+      <div className="light">
+        <div><span></span><span></span><span></span></div>
+        <div><span></span><span></span><span></span></div>
+        <div><span></span><span></span><span></span></div>
+        <div><span></span><span></span><span></span></div>
+        <div><span></span><span></span><span></span></div>
+        <div><span></span><span></span><span></span></div>
+        <div><span></span><span></span><span></span></div>
+        <div><span></span><span></span><span></span></div>
+        <div><span></span><span></span><span></span></div>
+        <div><span></span><span></span><span></span></div>
+        <div><span></span><span></span><span></span></div>
+        <div><span></span><span></span><span></span></div>
+      </div>
+    </div>
   </div>
 )
