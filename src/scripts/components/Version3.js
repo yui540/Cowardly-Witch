37eb4d8639c8261bbe8f6b9e7318aec8f8b16f09
yui540 <tabletennis540@gmail.com>
2018-08-13 14:17:38 +0900

    update: stage-1をマークアップ

--- src/scripts/components/Version3.js
+++ src/scripts/components/Version3.js
@@ -4,5 +4,31 @@ import VersionView from './VersionView'
 export default () => (
   <div className="version-3">
     <VersionView version="3" />
+    <div className="stage-1">
+      <div className="stage-1__bg">
+        <div className="small-ball"></div>
+        <div className="small-ball"></div>
+        <div className="small-ball"></div>
+        <div className="small-ball"></div>
+        <div className="big-ball"></div>
+      </div>
+      <div className="stage-1__frame">
+        <div></div><div></div><div></div><div></div>
+      </div>
+      <div className="stage-1__ball">
+        <div></div><div></div><div></div>
+      </div>
+      <div className="stage-1__box">
+        <div className="stage-1__box-effects">
+          <div></div><div></div><div></div>
+        </div>
+        <div className="stage-1__box-frame">
+          <div></div><div></div><div></div><div></div>
+        </div>
+      </div>
+      <div className="stage-1__end">
+        <div></div><div></div><div></div>
+      </div>
+    </div>
   </div>
 )
