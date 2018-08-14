c7b83b06c3f16779d1e24ed35ccd682803a6987e
yui540 <tabletennis540@gmail.com>
2018-08-14 14:11:57 +0900

    add: 汎用的なエフェクトをコンポーネント化

---
+++ src/scripts/components/effects/Effect1.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default ({ color1, color2, classname }) => (
+  <div className={`effect-1 ${classname}`}>
+    <div style={{ backgroundColor: color1 }}></div>
+    <div style={{ backgroundColor: color2 }}></div>
+  </div>
+)
---
+++ src/scripts/components/effects/Effect2.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export default ({ color, classname }) => (
+  <div className={`effect-2 ${classname}`}>
+    <div className="line">
+      <div><span style={{ backgroundColor: color }}></span></div>
+    </div>
+    <div className="line">
+      <div><span style={{ backgroundColor: color }}></span></div>
+    </div>
+    <div className="line">
+      <div><span style={{ backgroundColor: color }}></span></div>
+    </div>
+    <div className="line">
+      <div><span style={{ backgroundColor: color }}></span></div>
+    </div>
+    <div className="line">
+      <div><span style={{ backgroundColor: color }}></span></div>
+    </div>
+    <div className="line">
+      <div><span style={{ backgroundColor: color }}></span></div>
+    </div>
+    <div className="line">
+      <div><span style={{ backgroundColor: color }}></span></div>
+    </div>
+    <div className="line">
+      <div><span style={{ backgroundColor: color }}></span></div>
+    </div>
+  </div>
+)
