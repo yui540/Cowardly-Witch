ef8c9130f365b863a7dad83a7dd4a2874fd9399f
yui540 <tabletennis540@gmail.com>
2018-08-17 15:17:29 +0900

    update: stage-1をマークアップ

--- src/scripts/components/Version5.js
+++ src/scripts/components/Version5.js
@@ -4,5 +4,39 @@ import VersionView from './VersionView'
 export default () => (
   <div className="version-5">
     <VersionView version="5" />
+    <div className="stage-1">
+      <div className="stage-1__paint">
+        <div></div><div></div><div></div><div></div><div></div>
+      </div>
+      <div className="stage-1__paint-overray">
+        <div></div><div></div><div></div><div></div><div></div>
+      </div>
+      <div className="stage-1__box">
+        <div></div><div></div><div></div><div></div><div></div>
+      </div>
+      <div className="stage-1__bound-ball">
+        <div className="section">
+          <div></div><div></div><div></div><div></div><div></div>
+        </div>
+        <div className="section">
+          <div></div><div></div><div></div><div></div><div></div>
+        </div>
+        <div className="section">
+          <div></div><div></div><div></div><div></div><div></div>
+        </div>
+        <div className="section">
+          <div></div><div></div><div></div><div></div><div></div>
+        </div>
+        <div className="section">
+          <div></div><div></div><div></div><div></div><div></div>
+        </div>
+      </div>
+      <div className="stage-1__color">
+        <div></div><div></div><div></div><div></div><div></div>
+      </div>
+      <div className="stage-1__end">
+        <div></div><div></div><div></div><div></div>
+      </div>
+    </div>
   </div>
 )
