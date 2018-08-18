b6d0f89d43b896446fee3fd94075364938c5b3d6
yui540 <tabletennis540@gmail.com>
2018-08-18 19:43:43 +0900

    update: stage-2をマークアップ

--- src/scripts/components/Version5.js
+++ src/scripts/components/Version5.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import VersionView from './VersionView'
+import Effect1 from './effects/Effect1'
 
 export default () => (
   <div className="version-5">
@@ -37,6 +38,25 @@ export default () => (
       <div className="stage-1__end">
         <div></div><div></div><div></div><div></div>
       </div>
+    </div>
+    <div className="stage-2">
+      <div className="stage-2__box">
+        <div className="area area-1">
+          <Effect1 color1="#fff" color2="#C7D35E" />
+          <div className="eriri"></div>
+        </div>
+        <div className="area area-2">
+          <Effect1 color1="#fff" color2="#F2C047" />
+          <div className="eriri"></div>
+        </div>
+        <div className="area area-3">
+          <Effect1 color1="#fff" color2="#8FCDDA" />
+          <div className="eriri"></div>
+        </div>
+      </div>
+    </div>
+    <div className="stage-2__panel">
+      <div></div><div></div><div></div><div></div>
     </div>
   </div>
 )
