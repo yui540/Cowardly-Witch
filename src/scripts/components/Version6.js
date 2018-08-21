4610215fc5fb97b616d7a20e142bde69fe690822
yui540 <tabletennis540@gmail.com>
2018-08-21 10:58:49 +0900

    update: stage-1をマークアップ

---
+++ src/scripts/components/Version6.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import VersionView from './VersionView'
+import Effect2 from './effects/Effect2'
+
+export default ({ changeChapter }) => (
+  <div className="version-6">
+    <VersionView version="6" />
+    <div className="stage-1">
+      <div className="stage-1__box">
+        <div className="area area-1">
+          <div className="frame">
+            <div></div><div></div><div></div><div></div>
+          </div>
+          <Effect2 color="#fff" />
+          <div className="char">
+            <div></div><div></div><div></div><div></div><div></div>
+          </div>
+        </div>
+        <div className="area area-2">
+          <div className="frame">
+            <div></div><div></div><div></div><div></div>
+          </div>
+          <Effect2 color="#fff" />
+          <div className="char">
+            <div></div><div></div><div></div><div></div><div></div>
+          </div>
+        </div>
+        <div className="area area-3">
+          <div className="frame">
+            <div></div><div></div><div></div><div></div>
+          </div>
+          <Effect2 color="#fff" />
+          <div className="char">
+            <div></div><div></div><div></div><div></div><div></div>
+          </div>
+        </div>
+        <div className="area area-4">
+          <div className="frame">
+            <div></div><div></div><div></div><div></div>
+          </div>
+          <Effect2 color="#fff" />
+          <div className="char">
+            <div></div><div></div><div></div><div></div><div></div>
+          </div>
+        </div>
+      </div>
+      <div className="stage-1__shutter">
+        <div></div><div></div><div></div>
+      </div>
+      <div className="stage-1__shutter-2">
+        <div></div><div></div><div></div>
+      </div>
+    </div>
+    <div className="version-6__end">
+      <div></div><div></div><div></div><div></div><div></div>
+      <div onAnimationEnd={changeChapter}></div>
+    </div>
+  </div>
+)
