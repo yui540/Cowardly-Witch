e0077a16500369690e0f053fc6fb0239019061af
yui540 <tabletennis540@gmail.com>
2018-11-23 10:19:38 +0900

    fix: プロフィールの修正

--- src/scripts/components/ProfilePage.js
+++ src/scripts/components/ProfilePage.js
@@ -40,12 +40,12 @@ export default class ProfilePage extends Component {
             <div className="inner">
               <h4>状態</h4><p>病んでる</p>
               <h4>性別</h4><p>男</p>
-              <h4>ひとこと</h4><p>JavaScriptとCSSとHyperと可愛いのがすこ。</p>
-              <h4>誕生日</h4><p>1996/11/09 22歳</p>
+              <h4>ひとこと</h4><p>JavaScriptとCSSとHyperとAtomと可愛いのがすこ。</p>
+              <h4>誕生日</h4><p>1996/11/09 - 22歳</p>
               <h4>特徴</h4><p>病弱・臆病・ポンコツ・精神不安定</p>
               <h4>行動</h4><p>ずっとぐーたらしてる</p>
               <h4>好きな人・キャラ</h4><p>由比ヶ浜結衣, ミライアカリ, さユり, ナナヲアカリ, loundraw, 寺田てら, オーイシマサヨシ</p>
-              <h4>好きな技術</h4><p>JavaScript, CSS, Canvas2D, WebGL, Electron, Cordova</p>
+              <h4>好きな技術</h4><p>JavaScript, CSS, Canvas2D, WebGL, Electron, Node</p>
               <h4>好きな食べ物</h4><p>和菓子, 駄菓子, 肉じゃが, チョコレート</p>
               <h4>Kyash</h4><p>yui540</p>
               <h4 className="last">FANBOX</h4><p className="lastname"><a href="https://www.pixiv.net/fanbox/creator/16972511" target="_blank" rel="noopener noreferrer">yui540を応援する</a></p>
